6338d033a0763f4d0f0bddc493ec4e97b5ffb513
Admin <tahu2007@yandex.ru>
2022-03-10 00:32:24 +0300

    add file

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
-# This is a sample Python script.
 import openpyxl
 
+book = openpyxl.open('Firewall.xlsx', read_only=True)
 
-def example():
-    """
-    Один из способов чтения значений
-    :return:
-    """
-    book = openpyxl.load_workbook(filename="Firewall.xlsx")
-    # sheet = book.active
-    # sheet = book.worksheets[1]
-    sheet = book["Коллеги1"]
+sheet = book.active
+cells = sheet
+print(sheet [16][6].value)
 
-    # for row in sheet.values:
-    #     for cell in row:
-    #         print(cell)
-
-    for row in sheet.iter_rows():
-        for cell in row:
-            print(cell.value)
+for row in range (1,sheet.max_row+1):
+    src = sheet[row][3]
+    src_name = sheet[row][4]
+    src_ip = sheet[row][5]
+    print (row, src.value,src_ip.value,src_name.value)
