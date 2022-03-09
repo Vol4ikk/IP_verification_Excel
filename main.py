83ce6dbd172508a5928a1e648156f2e1f113e4ae
Admin <tahu2007@yandex.ru>
2022-03-10 00:07:37 +0300

    add file

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 # This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import openpyxl
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def example():
+    """
+    Один из способов чтения значений
+    :return:
+    """
+    book = openpyxl.load_workbook(filename="Firewall.xlsx")
+    # sheet = book.active
+    # sheet = book.worksheets[1]
+    sheet = book["Коллеги1"]
 
+    # for row in sheet.values:
+    #     for cell in row:
+    #         print(cell)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    for row in sheet.iter_rows():
+        for cell in row:
+            print(cell.value)
